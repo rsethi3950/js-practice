8d49a080e9303068c5e6c869028fbcf28dc09cec
Riya Sethi <52393286+rsethi3950@users.noreply.github.com>
2019-12-26 13:58:56 +0530

    Update form1.js

--- form1.js
+++ form1.js
@@ -1,10 +1,24 @@
 var form = document.getElementById('name-form');
+
 form.onsubmit = function(e){
 	e.preventDefault();
-	var result =  document.getElementById('result');
-	console.log(form.fullName.value);
+    console.log(form.uname.value);
 	console.log(form.email.value);
-	console.log(form.password.value);
-	console.log(form.number.value);
+	if(form.password.value.length<8){
+	console.log("password must be atleast 8 characters long.");
+    return true;
+    }
+    else if(form.password.value.length>=8){
+    	console.log(form.password.value);
+    }
+
+    if(form.number.value.length!=10){
+    	console.log("invalid contact");
+        return true;
+    }
+    else if(form.number.value.length==10){
+    	console.log(form.number.value);
+    }
 	form.reset();
 }
+
